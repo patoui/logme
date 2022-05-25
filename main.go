d73c9637ebb8cbf79be63802261f8d7cca821485
Patrique Ouimet <patrique.ouimet@gmail.com>
2022-05-25 17:48:12 -0400

    chore: fix docker daemonized setup

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to new server!")
+		fmt.Fprintf(w, "Welcome to LogMe!")
 	})
 
 	http.ListenAndServe(":8080", nil)
