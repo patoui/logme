7dc8af6262e128da7eb30f3298eee6cffffdf4f6
Patrique Ouimet <patrique.ouimet@gmail.com>
2024-05-02 20:11:37 -0400

    chore: update clickhouse version

--- docker/clickhouse/initdb/init.sql
+++ docker/clickhouse/initdb/init.sql
@@ -1,21 +1,21 @@
 CREATE DATABASE IF NOT EXISTS logs;
-
 CREATE TABLE IF NOT EXISTS logs.logs (
-    `uuid`       UUID DEFAULT generateUUIDv4(),
+    `uuid` UUID DEFAULT generateUUIDv4(),
     `account_id` UInt32,
-    `name`       String,
-    `dt`         DateTime,
-    `content`    String
-) engine=MergeTree()
+    `name` String,
+    `content` String,
+    `dt` DateTime,
+    `recorded_at` DateTime
+) engine = MergeTree()
 ORDER BY (account_id, dt);
 
 CREATE DATABASE IF NOT EXISTS logs_test;
-
 CREATE TABLE IF NOT EXISTS logs_test.logs (
-    `uuid`       UUID DEFAULT generateUUIDv4(),
+    `uuid` UUID DEFAULT generateUUIDv4(),
     `account_id` UInt32,
-    `name`       String,
-    `dt`         DateTime,
-    `content`    String
-) engine=MergeTree()
+    `name` String,
+    `content` String,
+    `dt` DateTime,
+    `recorded_at` DateTime
+) engine = MergeTree()
 ORDER BY (account_id, dt);
